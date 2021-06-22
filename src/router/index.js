d31d745de7491ad80c4efe952cab138cb74b5d5b
mengyi <chenmoonmo@gmail.com>
2021-06-22 09:43:20 +0800

    清理项目冗余 扩展vuex

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home'
 
 const routes = [
   {
@@ -7,18 +7,10 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 import { createStore } from 'vuex'
+import state from './state'
+import mutations from './mutations'
+import actions from './actions'
+import modules from './modules'
+
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+  mutations,
+  actions,
+  modules,
 })
